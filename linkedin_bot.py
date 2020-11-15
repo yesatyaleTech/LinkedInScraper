ce554c827be0b9ed3844cc49fe9750e1767d84f4
Ross Johnson <RossJ24@users.noreply.github.com>
2020-11-14 22:57:52 -0500

    Add profiling info

--- linkedin_bot.py
+++ linkedin_bot.py
@@ -3,15 +3,20 @@ import time
 from linkedinscraper.scraper import LinkedInScraper
 from linkedinscraper.spreadsheet import Spreadsheet
 
+
 def main():
     user = input('Please enter your Linkedin username\n\t ==>').strip()
     passw = input('Please enter your Linkedin password\n\t ==>').strip()
-    school = input('Please enter the name of the school you would like to search\n(Example: Yale-University)\n\t ==>').strip()
-    query = input('Please enter the attribute of the alumni that you are looking for\n\t ==>').strip()
-    driver_path = input('Please enter the file path of your browser driver\n\t ==>').strip()
-    excel_file_path = input('Please enter the desired path for your output spreadsheet\n\t ==>').strip()
+    school = input(
+        'Please enter the name of the school you would like to search\n(Example: Yale-University)\n\t ==>').strip()
+    query = input(
+        'Please enter the attribute of the alumni that you are looking for\n\t ==>').strip()
+    driver_path = input(
+        'Please enter the file path of your browser driver\n\t ==>').strip()
+    excel_file_path = input(
+        'Please enter the desired path for your output spreadsheet\n\t ==>').strip()
     before = time.time()
-    scraper = LinkedInScraper(user,passw,query,school,driver_path)
+    scraper = LinkedInScraper(user, passw, query, school, driver_path)
     scraper.gotoschool()
     scraper.search_people()
     scraper.get_names_and_urls()
@@ -19,12 +24,12 @@ def main():
     spreadsheet = Spreadsheet(excel_file_path)
     spreadsheet.fill_from_people(scraper.people)
     spreadsheet.save()
-    
 
     after = time.time()
     runtime = after - before
-    print('Number of Profiles assessed => ',)
-    print('Program Runtime => ',runtime,' seconds.')
+    print('Number of Profiles assessed => ', len(scraper.people))
+    print('Program Runtime => ', runtime, ' seconds.')
+
 
 if __name__ == "__main__":
     main()
